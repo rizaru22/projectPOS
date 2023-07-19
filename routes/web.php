5ddd47c04b3a55c470c1dace5cd79c7b1327128d
rizaru22 <rizaru.2.2@gmail.com>
2023-07-19 22:04:37 +0700

    kategori

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Http\Controllers\CategoryController;
+use App\Models\Category;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('dashboard',[
+        "title"=>"Dashboard"
+    ]);
+});
+
+Route::resource('kategori',CategoryController::class)->except('show','destroy','create');
