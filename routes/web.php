6d414f57203ea63844a7dbafd253326800b708b0
rizaru22 <rizaru.2.2@gmail.com>
2023-07-28 23:55:22 +0700

    manajemen user

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -27,4 +28,5 @@ Route::get('/', function () {
 Route::resource('kategori',CategoryController::class)->except('show','destroy','create');
 Route::resource('pelanggan',CustomerController::class)->except('destroy');
 Route::resource('produk',ProductController::class);
+Route::resource('pengguna',UserController::class)->except('destroy','create','show','update','edit');
 
